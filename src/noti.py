07cb7245f1d9a35d24c32a10bde9bcecca82ed80
ynj <ynj@nist.gov>
2014-09-18 17:03:57 -0400

    Minor changes

--- src/noti.py
+++ src/noti.py
@@ -1,11 +1,13 @@
 ## notification template
 import os
 def mail_me(mail_path='/usr/bin/mail', 
-
             subj='Parallel Job Finished',
             contents='Job Finished',
-            addr ='youngung.jeong@gmail.com'):
-    iflag = os.system("echo '%s' | %s -s '%s' %s"%(
-        contents, mail_path, subj, addr))
+            addr ='youngung.jeong@gmail.com',
+            attch = None):
+    cmd="echo '%s' | %s -s '%s' %s"%(
+        contents, mail_path, subj, addr)
+    print cmd
+    iflag = os.system(cmd)
     if iflag!=0: print 'Error in sending the notification'
     
